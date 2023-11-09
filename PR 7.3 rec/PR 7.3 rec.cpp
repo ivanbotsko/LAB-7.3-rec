e080d3b728a99ea465ceba8ebab9533f4e0873c2
ivanbotsko <ivan.botsko.ik.2023@lpnu.ua>
2023-11-09 10:27:46 +0200

    first master commit

---
+++ PR 7.3 rec/PR 7.3 rec.cpp
@@ -0,0 +1,149 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <climits>
+#include <time.h>
+using namespace std;
+
+void PrintRow(int** a, const int rowNo, const int colCount, int colNo)
+{
+    cout << setw(4) << a[rowNo][colNo];
+    if (colNo < colCount - 1)
+        PrintRow(a, rowNo, colCount, colNo + 1);
+    else
+        cout << endl;
+}
+
+void PrintRows(int** a, const int rowCount, const int colCount, int rowNo)
+{
+    PrintRow(a, rowNo, colCount, 0);
+    if (rowNo < rowCount - 1)
+        PrintRows(a, rowCount, colCount, rowNo + 1);
+    else
+        cout << endl;
+}
+
+void InputRow(int** a, const int rowNo, const int colCount, int colNo)
+{
+    cout << "a[" << rowNo << "][" << colNo << "] = ";
+    cin >> a[rowNo][colNo];
+    if (colNo < colCount - 1)
+        InputRow(a, rowNo, colCount, colNo + 1);
+    else
+        cout << endl;
+}
+
+void InputRows(int** a, const int rowCount, const int colCount, int rowNo)
+{
+    InputRow(a, rowNo, colCount, 0);
+    if (rowNo < rowCount - 1)
+        InputRows(a, rowCount, colCount, rowNo + 1);
+    else
+        cout << endl;
+}
+
+void CreateRow(int** a, const int rowNo, const int colCount,
+    const int Low, const int High, int colNo)
+{
+    a[rowNo][colNo] = Low + rand() % (High - Low + 1);
+    if (colNo < colCount - 1)
+        CreateRow(a, rowNo, colCount, Low, High, colNo + 1);
+}
+
+void CreateRows(int** a, const int rowCount, const int colCount,
+    const int Low, const int High, int rowNo)
+{
+    CreateRow(a, rowNo, colCount, Low, High, 0);
+    if (rowNo < rowCount - 1)
+        CreateRows(a, rowCount, colCount, Low, High, rowNo + 1);
+}
+
+int ProductOfNonNegativeRowsHelper(int** a, const int rowNo, const int colCount, int colNo)
+{
+    if (colNo < colCount)
+    {
+        if (a[rowNo][colNo] >= 0)
+        {
+            return a[rowNo][colNo] * ProductOfNonNegativeRowsHelper(a, rowNo, colCount, colNo + 1);
+        }
+        else
+        {
+            return ProductOfNonNegativeRowsHelper(a, rowNo, colCount, colNo + 1);
+        }
+    }
+    else
+    {
+        return 1; // base case, product initialized to 1
+    }
+}
+
+int ProductOfNonNegativeRows(int** a, const int rowCount, const int colCount, int rowNo)
+{
+    if (rowNo < rowCount)
+    {
+        int productForRow = ProductOfNonNegativeRowsHelper(a, rowNo, colCount, 0);
+        cout << "Product of non-negative elements in row " << rowNo << ": " << productForRow << endl;
+        return productForRow * ProductOfNonNegativeRows(a, rowCount, colCount, rowNo + 1);
+    }
+    else
+    {
+        return 1; // base case, product initialized to 1
+    }
+}
+
+int FindMaxSumDiagonalHelper(int** a, const int rowCount, const int colCount, int d, int i)
+{
+    if (i <= min(d, rowCount - 1))
+    {
+        int j = d - i;
+        return a[i][j] + FindMaxSumDiagonalHelper(a, rowCount, colCount, d, i + 1);
+    }
+    else
+    {
+        return 0; // base case, sum initialized to 0
+    }
+}
+
+int FindMaxSumDiagonal(int** a, const int rowCount, const int colCount, int d)
+{
+    if (d < rowCount + colCount - 1)
+    {
+        int sumForDiagonal = FindMaxSumDiagonalHelper(a, rowCount, colCount, d, max(0, d - rowCount + 1));
+        cout << "Sum for diagonal " << d << ": " << sumForDiagonal << endl;
+        return max(sumForDiagonal, FindMaxSumDiagonal(a, rowCount, colCount, d + 1));
+    }
+    else
+    {
+        return 0; // base case, max sum initialized to 0
+    }
+}
+
+int main()
+{
+    srand((unsigned)time(NULL));
+    int Low = -7;
+    int High = 7;
+    int rowCount, colCount;
+    cout << "rowCount = "; cin >> rowCount;
+    cout << "colCount = "; cin >> colCount;
+    int** a = new int* [rowCount];
+    for (int i = 0; i < rowCount; i++)
+        a[i] = new int[colCount];
+
+    // CreateRows(a, rowCount, colCount, Low, High, 0);
+    InputRows(a, rowCount, colCount, 0);
+    PrintRows(a, rowCount, colCount, 0);
+
+    // Call the recursive function for product of rows without negative elements
+    int product = ProductOfNonNegativeRows(a, rowCount, colCount, 0);
+    cout << "Product of rows without negative elements: " << product << endl;
+
+    // Call the recursive function for finding max sum among diagonals
+    int maxSum = FindMaxSumDiagonal(a, rowCount, colCount, 0);
+    cout << "Max sum of diagonals: " << maxSum << endl;
+
+    for (int i = 0; i < rowCount; i++)
+        delete[] a[i];
+    delete[] a;
+
+    return 0;
+}
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,38 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../PR 7.3 rec/PR 7.3 rec.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace YourMatrixFunctionsTests
+{
+    TEST_CLASS(ProductOfNonNegativeRowsTests)
+    {
+    public:
+
+        TEST_METHOD(TestProductOfNonNegativeRows)
+        {
+            const int rowCount = 3;
+            const int colCount = 4;
+
+            int** matrix = new int* [rowCount];
+            for (int i = 0; i < rowCount; i++)
+                matrix[i] = new int[colCount];
+
+            // Example matrix with rows that don't contain negative elements
+            matrix[0][0] = 1; matrix[0][1] = 2; matrix[0][2] = 3; matrix[0][3] = 4;
+            matrix[1][0] = 5; matrix[1][1] = 6; matrix[1][2] = 7; matrix[1][3] = 8;
+            matrix[2][0] = 9; matrix[2][1] = 10; matrix[2][2] = 11; matrix[2][3] = 12;
+
+            int product = ProductOfNonNegativeRows(matrix, rowCount, colCount, 0);
+
+            // The expected product is 1 * 2 * 3 * 4 * 5 * 6 * 7 * 8 * 9 * 10 * 11 * 12 = 479001600
+            Assert::AreEqual(479001600, product);
+
+            for (int i = 0; i < rowCount; i++)
+                delete[] matrix[i];
+            delete[] matrix;
+        }
+    };
+}
+
